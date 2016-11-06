d9155093850a927679b691afae10003e85a466b3
Heitor Pascoal de Bittencourt <heitorPB@users.noreply.github.com>
2016-11-06 17:37:25 -0200

    Minor firmware improvements

--- Firmware/src/EPR-LEF.ino
+++ Firmware/src/EPR-LEF.ino
@@ -1,43 +1,48 @@
-#include <Nanoshield_ADC.h>
+#include <Arduino.h>
 #include <Wire.h>
+// external libraries
+#include <Nanoshield_ADC.h>
 #include <SoftReset.h>
 
-long int i;
-int opcao = 'B';
-int media = 5;
-long int pot;
-int RESH; 
-int RESL;
-float x;
-float y;
-byte *data_x;
-byte *data_y;
 
 const int channel_x = 0;
 const int channel_y = 1;
-const int pinLed13 = 13;
+const int pinLed13  = 13;
+
 
 Nanoshield_ADC adc;
 
+
 void setup()
 {
+	adc.begin();
 	Serial.begin(115200);
+	while (!Serial);
 	pinMode(pinLed13, OUTPUT);
 	analogReference(DEFAULT);
-	media = 1;
-	adc.begin();
 }
 
- void loop()
+
+void loop()
 {
+	int opcao = 100;
+	int media = 5;
+	int i;
+	float x;
+	float y;
+	int RESH;
+	int RESL;
+	byte *data_x;
+	byte *data_y;
+
 	if (Serial.available() > 0) {
 		opcao = Serial.read();
-		//opcao= 'B';
-		switch(opcao)
-		{
+
+		switch(opcao) {
 		case 'A':
+			// TODO deixar esse case decente
 			ret1:
-			if (Serial.available( ) > 0)
+			if (Serial.available() > 0)
 				media = Serial.parseInt();
 			else
 				goto ret1;
@@ -46,10 +51,16 @@ void setup()
 			break;
 
 		case 'B':
+			// TODO separar isso aqui em duas funcoes. Ou Nao.
+			// pode nao ser uma boa ideia por questao de tempo
+			// de medida.
+			// TODO fazer um esquema de comunicacao decente,
+			// cf github.com/heitorPB/arduino-temp-logger
 			digitalWrite(pinLed13, HIGH);
+
 			x = 0;
 			y = 0;
-			for(i = 0; i < media; i++){  
+			for(i = 0; i < media; i++){
 				x += adc.readVoltage(channel_x);
 				y += adc.readVoltage(channel_y);
 				delay(2);
@@ -62,12 +73,12 @@ void setup()
 
 			/*RESH = int (x / 256);
 			Serial.write((byte)  RESH);
-			RESL = x - RESH * 256; 
+			RESL = x - RESH * 256;
 			Serial.write((byte)  RESL);
 
 			RESH = int (y / 256);
 			Serial.write((byte)  RESH);
-			RESL = y - RESH * 256; 
+			RESL = y - RESH * 256;
 			Serial.write((byte)  RESL);*/
 
 			data_x = (byte *) &x;
@@ -76,19 +87,17 @@ void setup()
 			data_y = (byte *) &y;
 			Serial.write(data_y, 4);
 
-			//Serial.println (pot); 
 			digitalWrite(pinLed13, LOW);
-
 			opcao = 100;
-			//opcao = 'B';
 			break;
 
 		case 'C':
-			opcao=100;
+			opcao = 100;
 			soft_restart();
-			opcao=100;
 			break;
 
+		default:
+			opcao = 100;
 		}
 	}
 }
